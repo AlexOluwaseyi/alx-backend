ceb52317e97b42abbb8290665ee91fcc309fcc81
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-05-15 15:15:27 +0100

    fixed locale check in app.config['LANGUAGES']

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -52,7 +52,8 @@ def get_locale():
         if user_locale in app.config['LANGUAGES']:
             return user_locale
     if g.user:
-        return g.user["locale"]
+        if g.user['locale'] in app.config['LANGUAGES']:
+            return g.user["locale"]
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
