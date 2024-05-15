ea7a1a37e724027a189a5d890dc0a1993345306c
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-05-15 10:17:47 +0100

    3-app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -17,14 +17,17 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
+babel = Babel(app)
 
 
+@babel.locale_selector
 def get_locale():
     """Get locale from request header """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel = Babel(app, locale_selector=get_locale)
+# babel = Babel(app, locale_selector=get_locale)
+# babel = Babel(app)
 
 
 @app.route("/", strict_slashes=False)
