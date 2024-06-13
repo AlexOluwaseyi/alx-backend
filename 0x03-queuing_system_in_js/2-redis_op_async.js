4080fe72083cd5644f5ee48705c17051d661805a
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-06-13 05:32:39 +0100

    redis

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,26 @@
+import { createClient, print } from 'redis';
+
+const util = require('util');
+
+const client = createClient();
+
+client.on('error', err => console.log('Redis client not connected to the server:', err.message));
+// client.on('connect', () => console.log('Redis client connected to the server'));
+
+function setNewSchool (schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+const displaySchoolValue = async (schoolName) => {
+  console.log(await util.promisify(client.GET).bind(client)(schoolName));
+};
+async function main() {
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+}
+
+client.on('connect', async () => {
+  console.log('Redis client connected to the server');
+  await main();
+});
--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,6 +1,8 @@
-import { createClient } from 'redis';
+import { createClient, print } from 'redis';
 
 const client = createClient();
 
 client.on('error', err => console.log('Redis client not connected to the server:', err.message));
 client.on('connect', () => console.log('Redis client connected to the server'));
+
+client.subscribe()
