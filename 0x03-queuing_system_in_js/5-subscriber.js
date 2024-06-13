eba3bf7067827901abbefd41ae1eb8390466eaef
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-06-13 05:47:30 +0100

    subscriber & publisher

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,8 +1,20 @@
-import { createClient, print } from 'redis';
+import { createClient } from 'redis';
 
 const client = createClient();
 
 client.on('error', err => console.log('Redis client not connected to the server:', err.message));
 client.on('connect', () => console.log('Redis client connected to the server'));
 
-client.subscribe()
+// client.subscribe()
+
+// Subscribe to the channel "holberton school channel"
+const channel = 'holberton school channel';
+client.subscribe(channel);
+
+client.on('message', (channel, message) => {
+  console.log(message);
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe(channel);
+    client.quit();
+  }
+});
